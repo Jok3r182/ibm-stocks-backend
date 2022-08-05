77e7915f936a11fd04b7b829eac81f405ba6dc8e
Matas <mataspauliusdregva@gmail.com>
2022-08-05 19:16:44 +0300

    - added web.config

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const helmet = require('helmet')
 const companyRoutes = require("./routes/company");
 const mongoConnect = require("./util/database").mongoConnect;
 
-const port = 80
+const port = process.env.PORT || 80
 const app = express();
 
 const swaggerUi = require('swagger-ui-express');
